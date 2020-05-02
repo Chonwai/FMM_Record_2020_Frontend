107d6e92beb708a07de79ff080b286ff2d227767
Chonwai <chonwaiun@gmail.com>
2020-05-02 17:34:49 +0800

    Add: Added the tailwind and axios.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import './plugins/axios'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/element.js';
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false;
 
