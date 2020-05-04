c68eb16eb0877bf09917bc1f34a9b9c3f8993454
Chonwai <chonwaiun@gmail.com>
2020-05-05 01:05:05 +0800

    Add: Added the button on Home page.

--- src/components/Global/SideNavigation.vue
+++ src/components/Global/SideNavigation.vue
@@ -1,11 +1,40 @@
 <template>
-    <aside id="side-navigation-component" class="h-screen w-64 bg-teal-800 fixed"></aside>
+    <aside id="side-navigation-component" class="h-screen w-64 bg-teal-800 fixed p-8">
+        <div class="p-8 mb-4">
+            <img src="@/assets/images/strategy.svg" alt="icon" srcset />
+        </div>
+        <div class="flex flex-col justify-center items-start w-full">
+            <router-link
+                v-for="(item, index) in functionList"
+                :key="index"
+                :to="{ name: item.project_name}"
+                class="nav-item w-full h-8 mb-2 flex flex-row items-center"
+            >
+                <img class="w-1/4" :src.sync="'@/assets/images/' + item.icon_name" />
+                <p class="w-3/4 flex justify-start text-white">{{ item.project_title }}</p>
+            </router-link>
+        </div>
+    </aside>
 </template>
 
 <script>
+// import Utils from '../../utils/Utils';
+import FunctionList from '@/data/FunctionList.json'
 export default {
     name: 'SideNavigation',
+    data() {
+        return {
+            functionList: FunctionList.function_list,
+        }
+    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::-webkit-scrollbar {
+    display: none;
+}
+
+.nav-item {
+}
+</style>
--- src/views/Home/HomeMain.vue
+++ src/views/Home/HomeMain.vue
@@ -1,9 +1,56 @@
 <template>
-    <div id="home-container"></div>
+    <div id="home-container" class="flex flex-wrap p-12">
+        <router-link
+            class="cards w-1/3 p-8"
+            v-for="(item, index) in functionList"
+            :key="index"
+            :to="{ name: item.project_name}"
+        >
+            <div class="card-icon mb-4">
+                <img :src="'@/assets/images/' + item.icon_name" alt="icon" />
+            </div>
+            <div class="card-title">
+                <p class="text-2xl font-bold">{{ item.project_title }}</p>
+            </div>
+        </router-link>
+    </div>
 </template>
 
 <script>
-export default {};
+import Utils from '../../utils/Utils';
+export default {
+    name: 'HomeMain',
+    data() {
+        return {
+            functionList: []
+        }
+    },
+    created() {
+        this.functionList = Utils.functionList();  
+    },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cards {
+    transition: 0.3s;
+    cursor: pointer;
+}
+
+.cards:hover {
+    transform: scale(1.2);
+    transition: 0.3s;
+    cursor: pointer;
+}
+
+.card-icon img {
+    width: 100%;
+    height: auto;
+}
+
+.card-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
